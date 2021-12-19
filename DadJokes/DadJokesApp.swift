398a2447ab5806e1bb7dc68351fd159ef6380995
alberto Juarbe <albertojuarbe2@gmail.com>
2021-12-19 18:02:21 -0500

    Initial Commit

---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Alberto Juarbe on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
